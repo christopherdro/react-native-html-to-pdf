c8125a9fb9afeb021ed52e7065fe097d7836ed14
Islam Salah <islamsalah007@gmail.com>
2017-11-16 11:55:53 +0200

    iText dependency has been removed only form android platform.

--- android/src/main/java/com/christopherdro/htmltopdf/RNHTMLtoPDFModule.java
+++ android/src/main/java/com/christopherdro/htmltopdf/RNHTMLtoPDFModule.java
@@ -7,33 +7,17 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.ReadableArray;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.ByteArrayInputStream;
 import java.util.UUID;
-import java.util.HashSet;
-import java.util.Set;
-import java.nio.charset.Charset;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.pdf.codec.Base64;
-import com.itextpdf.tool.xml.XMLWorkerHelper;
-import com.itextpdf.text.FontFactory;
-import com.itextpdf.tool.xml.XMLWorkerFontProvider;
 
 import android.os.Environment;
+import android.print.PdfConverter;
 
 public class RNHTMLtoPDFModule extends ReactContextBaseJavaModule {
 
-  private Promise promise;
   private final ReactApplicationContext mReactContext;
-  private Set<String> customFonts = new HashSet<>();
-
-  XMLWorkerFontProvider fontProvider = new XMLWorkerFontProvider(XMLWorkerFontProvider.DONTLOOKFORFONTS);
 
   public RNHTMLtoPDFModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -59,15 +43,6 @@ public class RNHTMLtoPDFModule extends ReactContextBaseJavaModule {
         fileName = UUID.randomUUID().toString();
       }
 
-      if (options.hasKey("fonts")) {
-        if (options.getArray("fonts") != null) {
-          final ReadableArray fonts = options.getArray("fonts");
-          for (int i = 0; i < fonts.size(); i++) {
-            customFonts.add(fonts.getString(i));
-          }
-        }
-      }
-
       if (options.hasKey("directory") && options.getString("directory").equals("docs")) {
         File path = new File(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOCUMENTS);
         if (!path.exists()) path.mkdir();
@@ -77,15 +52,9 @@ public class RNHTMLtoPDFModule extends ReactContextBaseJavaModule {
       }
 
       String filePath = convertToPDF(htmlString, destinationFile);
-      String base64 = "";
-
-      if (options.hasKey("base64") && options.getBoolean("base64") == true) {
-        base64 = Base64.encodeFromFile(filePath);
-      }
 
       WritableMap resultMap = Arguments.createMap();
       resultMap.putString("filePath", filePath);
-      resultMap.putString("base64", base64);
 
       promise.resolve(resultMap);
     } catch (Exception e) {
@@ -95,21 +64,8 @@ public class RNHTMLtoPDFModule extends ReactContextBaseJavaModule {
 
   private String convertToPDF(String htmlString, File file) throws Exception {
     try {
-      Document doc = new Document();
-      InputStream in = new ByteArrayInputStream(htmlString.getBytes());
-
-      PdfWriter pdf = PdfWriter.getInstance(doc, new FileOutputStream(file));
-
-      FontFactory.setFontImp(fontProvider);
-      for (String font : customFonts) {
-        fontProvider.register( font );
-      }
-
-      doc.open();
-      XMLWorkerHelper.getInstance().parseXHtml(pdf, doc, in, null, Charset.forName("UTF-8"), fontProvider);
-      doc.close();
-      in.close();
-
+      PdfConverter.getInstance()
+              .convert(mReactContext, htmlString, file);
       String absolutePath = file.getAbsolutePath();
 
       return absolutePath;
