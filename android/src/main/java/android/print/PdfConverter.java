2ee9c6179af23210c6a8fa375c3a4f0e912cb20f
mrxrinc <mrxrinc@users.noreply.github.com>
2018-08-25 15:01:07 +0430

    Update PdfConverter.java

--- android/src/main/java/android/print/PdfConverter.java
+++ android/src/main/java/android/print/PdfConverter.java
@@ -12,6 +12,7 @@ import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
 import android.webkit.WebSettings;
 import java.io.File;
 import android.util.Base64;
