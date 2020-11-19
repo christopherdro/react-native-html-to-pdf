ec069fb9e6a515500fd47a59af07154fe2f80a3a
rohitatcoffee <rohit_deshmukh11@yahoo.com>
2020-11-19 14:48:26 +0800

    Update PdfConverter.java

--- android/src/main/java/android/print/PdfConverter.java
+++ android/src/main/java/android/print/PdfConverter.java
@@ -96,7 +96,7 @@ public class PdfConverter implements Runnable {
         });
         WebSettings settings = mWebView.getSettings();
         settings.setDefaultTextEncodingName("utf-8");
-        mWebView.loadData(mHtmlString, "text/HTML; charset=utf-8", null);
+        mWebView.loadDataWithBaseURL(mBaseURL, mHtmlString, "text/HTML", "utf-8", null);
     }
 
     public PrintAttributes getPdfPrintAttrs() {
