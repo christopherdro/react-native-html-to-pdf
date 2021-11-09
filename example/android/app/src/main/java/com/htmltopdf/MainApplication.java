7ed58d32b2e26ee683a1dc69aaae60d3533c4ad0
Mickael Lecoq <mickael@lecoq.me>
2021-11-09 17:43:49 +0100

    fix: link example to lib (inspired by react-native-builder-bob)

--- example/android/app/src/main/java/com/htmltopdf/MainApplication.java
+++ example/android/app/src/main/java/com/htmltopdf/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new RNHTMLtoPDFPackage());
           return packages;
         }
 
